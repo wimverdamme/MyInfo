e023b1dd4499598f74a81e2d3f2c78e805b83f4d
Gijs Paepen <gijs.paepen@kla-tencor.com>
2014-05-09 11:49:10 +0200

    Removed gpx logging

--- src/my/info/MyInfoActivity.java
+++ src/my/info/MyInfoActivity.java
@@ -6,16 +6,14 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.ObjectInputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.zip.GZIPInputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
-
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -33,13 +31,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import my.info.Poi;
 
+@SuppressLint("DefaultLocale")
 public class MyInfoActivity extends Activity {
 
 	/** Called when the activity is first created. */
@@ -49,7 +45,6 @@ public class MyInfoActivity extends Activity {
 		setContentView(R.layout.main);
 		mPrefs = getPreferences(MODE_PRIVATE);
 		DebugMode = mPrefs.getBoolean("DebugMode", false);
-		DaysBack = mPrefs.getInt("DaysBack", 0);
 		mContext=this;
 		if (PoiList.isEmpty())
 			Convert();
@@ -64,17 +59,15 @@ public class MyInfoActivity extends Activity {
 		intent.putExtra(FileDialog.CAN_SELECT_DIR, true);
 		intent.putExtra(FileDialog.SELECT_ONLY_DIR, true);
 
-		if (view.getId() == R.id.ExportButton)
-			startActivityForResult(intent, REQUEST_EXPORT);
-		else if (view.getId() == R.id.SaveButton)
+		if (view.getId() == R.id.SaveButton)
 			startActivityForResult(intent, REQUEST_SAVE);
 	}
 
 	static Context mContext;
-	static final int REQUEST_EXPORT = 1;
+	
 	static final int REQUEST_SAVE = 2;
 	Dialog popUp;
-	int DaysBack=0;
+	
 	public synchronized void onActivityResult(final int requestCode,
 			int resultCode, final Intent data) {
 
@@ -82,46 +75,11 @@ public class MyInfoActivity extends Activity {
 			String Action = "";
 			if (requestCode == REQUEST_SAVE) {
 				Action = "Saving to: ";
-			} else if (requestCode == REQUEST_EXPORT) {
-				Action = "Exporting to: ";
-			}					
+			}				
 
 			String filePath = data.getStringExtra(FileDialog.RESULT_PATH);
 			Toast.makeText(this, Action + filePath, Toast.LENGTH_SHORT).show();
-			if (requestCode == REQUEST_EXPORT) {
-				File[] files = new File(getCacheDir(), "").listFiles();
-				for (File f : files) {
-					String filename = f.getName();
-					if (filename.endsWith(".sav") && (DaysBack==0 || f.lastModified()+DaysBack*1000*60*60*24>(new Date()).getTime())) {
-						String trackName = filename.substring(0,
-								filename.length() - 4);
-						try {
-							ArrayList<RunningActivity.Point> points = new ArrayList<RunningActivity.Point>();
-							ObjectInputStream is = new ObjectInputStream(
-									new GZIPInputStream(new FileInputStream(
-											getCacheDir() + "/" + filename)));
-							while (is.available() > 0) {
-
-								points.add(new RunningActivity.Point(is
-										.readInt(), is.readInt(), is
-										.readFloat(), is.readFloat(), is
-										.readLong()));
-							}
-							is.close();
-
-							File gpxFile = new File(filePath + "/" + trackName
-									+ ".gpx");
-							GPXFileWriter.writeGpxFile(trackName, points,
-									gpxFile);
-
-						} catch (Exception e) {
-							Log.i("MyInfoActivity",
-									"onActivityResult() export writeGpxFile "
-											+ e.getMessage());
-						}
-					}
-				}
-			}
+			
 			if (requestCode == REQUEST_SAVE) {
 				File[] files = new File(getCacheDir(), "").listFiles();
 				for (File f : files) {
@@ -323,9 +281,9 @@ public class MyInfoActivity extends Activity {
 	}
 
 	final int DebugModeMenuItemId = 0;
-	final int HistoryItemId=1;
-	final int AboutItemId = 2;
-	final int ExitMenuItemId = 3;
+
+	final int AboutItemId = 1;
+	final int ExitMenuItemId = 2;
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -334,7 +292,6 @@ public class MyInfoActivity extends Activity {
 		DebugModeItem.setCheckable(true);
 		DebugModeItem.setChecked(DebugMode);
 
-		menu.add(Menu.NONE, HistoryItemId, Menu.NONE, R.string.HistoryDays);
 		
 		menu.add(Menu.NONE, AboutItemId, Menu.NONE, R.string.About);
 
@@ -351,44 +308,7 @@ public class MyInfoActivity extends Activity {
 			DebugMode = !item.isChecked();
 			item.setChecked(DebugMode);
 			break;
-		case HistoryItemId:
-			EditText et;
-			Button button;
-			popUp = new Dialog(MyInfoActivity.this);
-			popUp.setContentView(R.layout.popupdialog);
-			popUp.setTitle("How Many days back:");
-			popUp.setCancelable(true);
-
-			et = (EditText) popUp.findViewById(R.id.Value);
-			et.setInputType(android.text.InputType.TYPE_CLASS_NUMBER);
-			et.setText("0");
-
-			button = (Button) popUp.findViewById(R.id.CloseButton);
-			button.setOnClickListener(new OnClickListener() {
-
-				public void onClick(View v) {
-					popUp.dismiss();
-				}
-			});
-			button = (Button) popUp.findViewById(R.id.OkButton);
-			button.setOnClickListener(new OnClickListener() {
-
-				public void onClick(View v) {
-					View parent = (View) v.getParent();
-					String Info = ((TextView) parent.findViewById(R.id.Value))
-							.getText().toString();
-					try {
-						DaysBack = Integer.parseInt(Info);
-					} catch (NumberFormatException e) {
-						Log.i("RunningActivity", "Warning secs NumberFormatException - "+e.getMessage());
-					}
-
-					popUp.dismiss();
-				}
-			});
-
-			popUp.show();
-			break;
+		
 		case AboutItemId:
 			AlertDialog.Builder alertbox = new AlertDialog.Builder(this);
 			PackageInfo packageInfo = new PackageInfo();
@@ -407,6 +327,7 @@ public class MyInfoActivity extends Activity {
 				ZipEntry ze = zf.getEntry("classes.dex");
 
 				BuildDate = ze.getTime();
+				zf.close();
 			} catch (Exception e) {
 
 			}
@@ -437,7 +358,6 @@ public class MyInfoActivity extends Activity {
 		super.onPause();
 		SharedPreferences.Editor ed = mPrefs.edit();
 		ed.putBoolean("DebugMode", DebugMode);
-		ed.putInt("DaysBack", DaysBack);
 		ed.commit();
 	}
 
